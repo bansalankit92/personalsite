d56744e4939d65fa3ae3feb8adf0b55f738dae95
abansal <bansal.ankit92@gmail.com>
2019-07-10 17:39:49 +0530

    added new image and updated spelling mistakes

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@
 				restrict: 'A',
 				link: function (scope, element, attr) {
 					element.typed({
-						strings: [ " Full Stack Developer", " Blockchain Enthusiast"," Solopreneur"," Ankit Bansal"],
+						strings: [ " Tech Enthusiast", " Software Developer", " Problem Solver"," Ankit Bansal"],
 						typeSpeed: 1,
 						// time before typing starts
 						startDelay: 200,
